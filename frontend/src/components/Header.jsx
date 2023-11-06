ea801e9fa020e676aeb9219fed8813ebba10d1cc
anilsebastin <anilsebastin@gmail.com>
2023-11-06 12:49:36 +0530

    Fixed small bugs

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -6,6 +6,7 @@ import { useNavigate } from "react-router-dom";
 import { useLogoutMutation } from "../slices/usersApiSlice";
 import { logout } from "../slices/authSlice";
 import SearchBox from "./SearchBox";
+import { resetCart } from "../slices/cartSlice";
 
 import logo from "../assets/logo.png";
 
@@ -23,6 +24,7 @@ const Header = () => {
     try {
       await logoutApiCall().unwrap();
       dispatch(logout());
+      dispatch(resetCart());
       navigate("/login");
     } catch (err) {
       console.error(err);
