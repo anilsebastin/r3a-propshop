e822805aa359eeb97b7804688b5f59be62d7d050
anilsebastin <anilsebastin@gmail.com>
2023-11-06 10:30:08 +0530

    Product carousel

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -138,8 +138,15 @@ const createProductReview = asyncHandler(async (req, res) => {
     throw new Error("Product not found");
   }
 });
-// Section 12:Reviews Search and more
-// Product reviews @ GIT
+
+// @desc    Get top rated products
+// @route   GET /api/products/top
+// @access  Public
+const getTopProducts = asyncHandler(async (req, res) => {
+  const products = await Product.find({}).sort({ rating: -1 }).limit(3);
+
+  res.json(products);
+});
 
 export {
   getProducts,
@@ -148,4 +155,5 @@ export {
   updateProduct,
   deleteProduct,
   createProductReview,
+  getTopProducts,
 };
